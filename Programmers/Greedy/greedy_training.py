10e011c493a9af306e3c6e8484bd891ff88fc9f3
zikx <8687706@gmail.com>
2020-04-01 01:39:51 +0900

    grrdy/training

---
+++ Programmers/Greedy/greedy_training.py
@@ -0,0 +1,28 @@
+# 프로그래머스 
+# Greedy level 1 체육복
+
+def solution(n, lost, reserve):
+    answer = 0
+    # have = [1] * n
+    # set = 중복을 허용하지 않는 자료형
+    # list 와 비교했을 때 연산이 가능
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for i in set_reserve:
+        if i - 1 in set_lost:
+            set_lost.remove(i-1)
+        elif i + 1 in set_lost:
+            set_lost.remove(i+1)
+    answer = n - len(set_lost)
+
+    return answer
+
+n = 5 
+lost = [2,4]
+reserve = [3]
+
+# set 자료형은 중복 불가의 자료형 
+# type = set
+# 객체간의 연산 가능
+
+print(solution(n, lost, reserve))
