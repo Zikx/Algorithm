10cc04b2896557ed596c4f395263fda183013423
zikx <8687706@gmail.com>
2020-07-14 19:41:57 +0900

    camouflage

---
+++ Programmers/Hash/위장.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+def solution(clothes):
+    category_clothes = Counter([category for name, category in clothes])
+    cnt = 1
+    for values in category_clothes.values():
+        cnt *= values + 1
+
+    return cnt - 1
+
+
+clothes1 = [["yellow_hat", "headgear"], 
+            ["blue_sunglasses", "eyewear"],
+            ["green_turban", "headgear"],
+            ]
+
+clothes2 = [["crow_mask", "face"], 
+            ["blue_sunglasses", "face"], 
+            ["smoky_makeup", "face"]]
+
+
+# print(solution(clothes1)) # return 5
+print(solution(clothes2)) # return 3
