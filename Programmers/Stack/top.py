a2df111a6943873a8300ab7ff732cf7864ce6c72
zikx <8687706@gmail.com>
2020-07-14 22:37:20 +0900

    top

--- Programmers/Stack/top.py
+++ Programmers/Stack/top.py
@@ -1,11 +1,19 @@
 def solution(heights):
-    answer = []
-    for i in range(len(heights)):
-        # print(heights.pop())
-        prev = heights.pop()
-        if 
-    # return answer
+    answer = [0] * len(heights)
 
-heights_s = [3,9,9,3,5,7,2]
+    while heights:
+        element = heights.pop()
+        for e in range(len(heights) - 1, -1, -1):
+            if element < heights[e]:
+                answer[len(heights)] = e + 1
+                break
 
-print(solution(heights_s))
+    return answer
+
+
+
+heights1 = [3,9,9,3,5,7,2] # 0,0,2,2,4
+heights2 = [6,9,5,7,4] # 0,0,0,3,3,3,6
+heights3 = [1,5,3,6,7,6,5] # 0,0,2,0,0,5,6
+
+print(solution(heights2))
