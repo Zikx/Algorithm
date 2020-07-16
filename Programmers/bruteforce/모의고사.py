95f2a4533bbd8c7d407b05919a4c1bbaf4e1a93a
zikx <8687706@gmail.com>
2020-07-16 17:52:20 +0900

    모의고사

--- Programmers/bruteforce/모의고사.py
+++ Programmers/bruteforce/모의고사.py
@@ -1,7 +1,34 @@
+# 1번 1,2,3,4,5 ... (5개)
+# 2번 2,1,2,3,2,4,2,5 ....(8개)
+# 3번 3,3,1,1,2,2,4,4,5,5 .. (10개)
+
 def solution(answers):
-    answer = []
-    len_answer = len(answer)
+    re_answer = list()
+    answer = [0,0,0]
+    answer_len = len(answers)
+    num1 = [1,2,3,4,5]
+    num2 = [2,1,2,3,2,4,2,5]
+    num3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    for i in range(answer_len):
+        if num1[i % (len(num1))] == answers[i]:
+            answer[0] += 1
+        if num2[i % (len(num2))] == answers[i]:
+            answer[1] += 1
+        if num3[i % (len(num3))] == answers[i]:
+            answer[2] += 1
+    
+    max_answers = max(answer)
 
-    return answer
+    for i in range(len(answer)):
+        if max_answers == answer[i]:
+            re_answer.append(i+1)
 
-print(solution(answers))
+    re_answer.sort()
+    return re_answer
+
+answers1 = [1,2,3,4,5] # 1
+answers2 = [1,3,2,4,2] # 1,2,3
+
+
+print(solution(answers2))
