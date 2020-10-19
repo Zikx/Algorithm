ace9eaaa2ba9992c575359630c64b408f3e789b6
Zikx <8687706@gmail.com>
2020-10-19 17:01:54 +0900

    Update maraton

--- Programmers/Hash/maraton.py
+++ Programmers/Hash/maraton.py
@@ -1,26 +1,11 @@
-# import collections
-
-# def solution(participant, completion):
-#     answer_counter = collections.Counter(participant) - collections.Counter(completion)
-#     answer = list(answer_counter.keys())[0]
-#     return answer
-
-
-# participant = ["leo","kiki", "eden"]
-# completion = ["eden", "kiki"]
-
-
-# print(solution(participant, completion))
-
-participant = ["leo","kiki", "eden"]
-completion = ["eden", "kiki"]
-
-participant2 = ["mislav", "stanko", "mislav", "ana"]
-completion2 = ["stanko", "ana", "mislav"]
-
 from collections import Counter
 
 def solution(participant, completion):
     return list((Counter(participant) - Counter(completion)).keys())[0]
 
-print(solution(participant2, completion2))
+participant = ["leo","kiki", "eden", "leo"]
+completion = ["eden", "kiki"]
+participant2 = ["mislav", "stanko", "mislav", "ana","titi"]
+completion2 = ["stanko", "ana", "mislav"]
+
+print(solution(participant2,completion2))
